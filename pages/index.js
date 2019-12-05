8bd85b0d8b28207972462b4ec608da44fb70b0f6
alex <alexandre.fourcat@epitech.eu>
2019-12-05 17:31:09 +0100

    Testing index.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
+import InfoBox from '../components/infobox'
 
 const Home = () => (
   <div>
@@ -8,34 +9,10 @@ const Home = () => (
       <title>Home</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-
-    <Nav />
-
+    <Nav name="Home" user={{ name: 'roger' }} />
     <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
+      <InfoBox title={'Test'} content={'Bonjour 123'} p={2} m={2} button={'more'} />
     </div>
-
     <style jsx>{`
       .hero {
         width: 100%;
