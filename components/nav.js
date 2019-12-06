5586943f05d93dba773dfec45d8cca56a9a595cb
Quentin <quentin.liardeaux@epitech.eu>
2019-12-06 16:36:51 +0100

    Add logo svg file, WIP: Add nav bar

--- components/nav.js
+++ components/nav.js
@@ -1,55 +1,90 @@
 import React from 'react'
 import Link from 'next/link'
 
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
-
 const Nav = () => (
   <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
-    </ul>
+    <div className="image">
+      <img src="logo-brain.svg"/>
+    </div>
+    <div className="nav-bar">
+      <div className="content">
+        <a href="#">Products</a>
+        <a href="#">Partners</a>
+        <a href="#">Community</a>
+        <a href="#">Pricing</a>
+      </div>
+      <div className="content">
+        <a href="#" className="signin">Sign in</a>
+        <a href="#" className="signup">Sign up</a>
+      </div>
+    </div>
 
     <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
       nav {
-        text-align: center;
-      }
-      ul {
         display: flex;
         justify-content: space-between;
+        background-color: #1A95FF;
+        color: white;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 300;
+        font-style: italic;
+        height: 200px;
       }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
+
+      .nav-bar {
         display: flex;
-        padding: 6px 8px;
+        height: 50px;
+        justify-content: space-between;
+        align-items: center;
       }
+
       a {
-        color: #067df7;
+        display: inline-block;
+        padding: 6px 8px;
+        list-style-type: none;
+        font-size: 17px;
         text-decoration: none;
-        font-size: 13px;
+        text-align: center;
+        color: white;
       }
-    `}</style>
+
+      a:active {
+        color: black;
+      }
+
+      .login-bar {
+        display: flex;
+      }
+
+      .signin {
+        background-color: #1A95FF;
+        border: 0px solid white;
+      }
+
+      .signup {
+        width: 100px;
+        border-radius: 16px;
+        border: 3px solid white;
+        background-color: #1A95FF;
+      }
+
+      .signup:hover {
+        background-color: white;
+        color: #1A95FF;
+        margin-left: auto;
+        margin-right: auto
+      }
+      .image {
+        display: inline-block;
+        width: 90px;
+        height: 90px;
+      }
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+   `}</style>
   </nav>
 )
 
